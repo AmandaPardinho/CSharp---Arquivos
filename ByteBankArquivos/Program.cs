22c7749a60c28c45bc54e6c40efabdb870414a59
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-20 12:44:39 -0300

    Alteração no método Main - definição da classe principal como partial e código realocado para outro item

--- ByteBankArquivos/Program.cs
+++ ByteBankArquivos/Program.cs
@@ -1,32 +1,40 @@
-﻿class Program
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
         var enderecoArquivo = "contas.txt";
-        var numeroBytesLidos = -1;
-        var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
 
-        /* public override int Read(byte[] array, int offset, int count);
-         * byte[] array => buffer
-         * ** armazena os bytes lidos pelo método
-         * ** pode ser reutilizado para guardar informações temporárias
-         * ** 1024 => tamanho de um kbyte
-         * int offset => delimita o índice em que o método vai começar a preencher o array
-         * int count => informa o número de posições a serem preenchidas
-         * o método Read devolve o número total de bytes lidos do buffer, podendo ser menor que o número de bytes solicitado se o número de bytes não estiver disponível no momento, ou for igual a zero se o final do fluxo for atingido 
-         */
-
-        var buffer = new byte[1024];
-
-        while(numeroBytesLidos != 0)
+        using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open))
         {
-            numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+            var numeroBytesLidos = -1;
 
-            EscreverBuffer(buffer);
+            /* public override int Read(byte[] array, int offset, int count);
+             * byte[] array => buffer
+             * ** armazena os bytes lidos pelo método
+             * ** pode ser reutilizado para guardar informações temporárias
+             * ** 1024 => tamanho de um kbyte
+             * int offset => delimita o índice em que o método vai começar a preencher o array
+             * int count => informa o número de posições a serem preenchidas
+             * o método Read devolve o número total de bytes lidos do buffer, podendo ser menor que o número de bytes solicitado se o número de bytes não estiver disponível no momento, ou for igual a zero se o final do fluxo for atingido 
+             */
 
-        }
+            var buffer = new byte[1024];
+            //var tempo = new Stream(fluxoArquivo);
 
-        Console.ReadKey();
+            while (numeroBytesLidos != 0)
+            {
+                numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+                Console.WriteLine($"Bytes lidos: {numeroBytesLidos}");
+                EscreverBuffer(buffer, numeroBytesLidos);
+            }
+
+            fluxoArquivo.Close();
+            Console.ReadKey();
+        }       
     }
 
     private static object ContaCorrente(int v1, int v2)
@@ -34,12 +42,16 @@
         throw new NotImplementedException();
     }
 
-    static void EscreverBuffer(byte[] buffer)
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
-        foreach (var meuByte in buffer)
+        var utf8 = new UTF8Encoding();
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+
+        /*foreach (var meuByte in buffer)
         {
             Console.Write(meuByte);
             Console.Write(" ");
-        }
+        }*/
     }
 }
