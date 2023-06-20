c50253e1d9307750b0d14d7b05110a17d8640dea
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-20 18:23:35 -0300

    Criação de um novo método para criar contas

--- ByteBankArquivos/Program.cs
+++ ByteBankArquivos/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using ByteBankArquivos;
 
 partial class Program
 {
@@ -24,11 +25,35 @@ partial class Program
             while(!leitor.EndOfStream)
             {
                 var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"Número da conta: {contaCorrente.Numero}; Agência: {contaCorrente.Agencia}; Saldo: {contaCorrente.Saldo}; Nome do titular: {contaCorrente.Titular}";
+                Console.WriteLine(msg);
             }
-
-
         } 
         Console.ReadKey();        
+    }
+
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        var campos = linha.Split(',');
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2].Replace('.', ',');
+        var nomeTitular = campos[3];
+
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt = int.Parse(numero);
+        var saldoComoDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComoDouble);
+        resultado.Titular = titular;
+
+        return resultado;
     }
 }
