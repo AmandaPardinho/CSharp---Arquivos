d7e3b337c9b7ae72016eac13756c3df45a0f6166
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-20 17:18:27 -0300

    Métodos que podem ser usados para ler arquivos

--- ByteBankArquivos/Program.cs
+++ ByteBankArquivos/Program.cs
@@ -2,56 +2,33 @@
 using System.IO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
         var enderecoArquivo = "contas.txt";
-
         using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open))
         {
-            var numeroBytesLidos = -1;
+            var leitor = new StreamReader(fluxoArquivo);
 
-            /* public override int Read(byte[] array, int offset, int count);
-             * byte[] array => buffer
-             * ** armazena os bytes lidos pelo método
-             * ** pode ser reutilizado para guardar informações temporárias
-             * ** 1024 => tamanho de um kbyte
-             * int offset => delimita o índice em que o método vai começar a preencher o array
-             * int count => informa o número de posições a serem preenchidas
-             * o método Read devolve o número total de bytes lidos do buffer, podendo ser menor que o número de bytes solicitado se o número de bytes não estiver disponível no momento, ou for igual a zero se o final do fluxo for atingido 
-             */
+            /*var linha = leitor.ReadLine();
+            Console.WriteLine(linha);
+            Console.WriteLine("");*/
 
-            var buffer = new byte[1024];
-            //var tempo = new Stream(fluxoArquivo);
+            /*var texto = leitor.ReadToEnd();
+            Console.WriteLine(texto);*/
 
-            while (numeroBytesLidos != 0)
+            /*var numero = leitor.Read(); // traz o número do primeiro caractere em byte
+            Console.WriteLine(numero);*/
+
+            while(!leitor.EndOfStream)
             {
-                numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
-                Console.WriteLine($"Bytes lidos: {numeroBytesLidos}");
-                EscreverBuffer(buffer, numeroBytesLidos);
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
             }
 
-            fluxoArquivo.Close();
-            Console.ReadKey();
-        }       
-    }
 
-    private static object ContaCorrente(int v1, int v2)
-    {
-        throw new NotImplementedException();
-    }
-
-    static void EscreverBuffer(byte[] buffer, int bytesLidos)
-    {
-        var utf8 = new UTF8Encoding();
-        var texto = utf8.GetString(buffer, 0, bytesLidos);
-        Console.Write(texto);
-
-        /*foreach (var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
-        }*/
+        } 
+        Console.ReadKey();        
     }
 }
