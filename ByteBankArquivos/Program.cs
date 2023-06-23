772a5089e872958f95e7e7d55f97e73b1d83bcfb
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-23 17:49:45 -0300

    Testando métodos da classe File

--- ByteBankArquivos/Program.cs
+++ ByteBankArquivos/Program.cs
@@ -9,7 +9,20 @@ namespace ByteBankArquivos
     {
         static void Main(string[] args)
         {
-            UsarStreamEntrada();
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach (var linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+            Console.WriteLine();
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"O arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
             Console.WriteLine("Aplicação Finalizada");
 
             Console.ReadLine();
