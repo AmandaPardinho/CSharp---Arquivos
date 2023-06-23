2b28484a16afdb83cd3e14046eacd8b09947b446
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-23 13:34:45 -0300

    Teste do código para criar o arquivo csv

--- ByteBankArquivos/Program.cs
+++ ByteBankArquivos/Program.cs
@@ -3,57 +3,14 @@ using System.IO;
 using System.Text;
 using ByteBankArquivos;
 
-partial class Program
+namespace ByteBankArquivos
 {
-    static void Main(string[] args)
+    partial class Program
     {
-        var enderecoArquivo = "contas.txt";
-        using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open))
+        static void Main(string[] args)
         {
-            var leitor = new StreamReader(fluxoArquivo);
-
-            /*var linha = leitor.ReadLine();
-            Console.WriteLine(linha);
-            Console.WriteLine("");*/
-
-            /*var texto = leitor.ReadToEnd();
-            Console.WriteLine(texto);*/
-
-            /*var numero = leitor.Read(); // traz o número do primeiro caractere em byte
-            Console.WriteLine(numero);*/
-
-            while(!leitor.EndOfStream)
-            {
-                var linha = leitor.ReadLine();
-                var contaCorrente = ConverterStringParaContaCorrente(linha);
-
-                var msg = $"Número da conta: {contaCorrente.Numero}; Agência: {contaCorrente.Agencia}; Saldo: {contaCorrente.Saldo}; Nome do titular: {contaCorrente.Titular.Nome}\n";
-                Console.WriteLine(msg);
-            }
-        } 
-        Console.ReadKey();        
-    }
-
-    static ContaCorrente ConverterStringParaContaCorrente(string linha)
-    {
-        var campos = linha.Split(',');
-
-        var agencia = campos[0];
-        var numero = campos[1];
-        var saldo = campos[2].Replace('.', ',');
-        var nomeTitular = campos[3];
-
-        var agenciaComInt = int.Parse(agencia);
-        var numeroComInt = int.Parse(numero);
-        var saldoComoDouble = double.Parse(saldo);
-
-        var titular = new Cliente();
-        titular.Nome = nomeTitular;
-
-        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
-        resultado.Depositar(saldoComoDouble);
-        resultado.Titular = titular;
-
-        return resultado;
+            CriarArquivo();
+            Console.ReadKey();
+        }
     }
 }
