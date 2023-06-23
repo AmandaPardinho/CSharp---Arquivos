319a7de1581543ed3793f4355aa1e6e60191f3f8
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-23 16:11:59 -0300

    Criação dos métodos CriarArquivoComWriter e TestaEscrita

--- ByteBankArquivos/3_CriandoArquivo.cs
+++ ByteBankArquivos/3_CriandoArquivo.cs
@@ -23,5 +23,33 @@ namespace ByteBankArquivos
                 fluxoArquivo.Write(bytes, 0, bytes.Length);
             }
         }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoNovoArquivo = "contasExportadas2.csv";
+
+            using(var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using(var escritor = new StreamWriter(fluxoArquivo))
+            {
+                escritor.Write("456, 65465, 456.0, Pedro");
+            }
+        }
+
+        static void TestaEscrita()
+        {
+            var caminhoNovoArquivo = "teste.txt";
+
+            using (var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoArquivo))
+            {
+                for(var i = 0; i < 1000000; i++)
+                {
+                    escritor.WriteLine($"Linha {i}");
+                    escritor.Flush(); //Despeja o buffer para o Stream
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter...");
+                    Console.ReadLine();
+                }
+            }
+        }
     }
 }
