a99632e35b89d84709f5fc2e7bc6ed941d1d83f8
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-23 13:31:32 -0300

    Criado código para gerar um novo arquivo csv

---
+++ ByteBankArquivos/3_CriandoArquivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankArquivos
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using(var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456, 7895, 4785.40, Gustavo Santos";
+
+                var encoding = Encoding.UTF8;
+
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
